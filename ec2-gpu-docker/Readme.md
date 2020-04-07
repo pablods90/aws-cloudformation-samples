66025756cbafe85db47f084f37f3a250651c105b
Pablo Andres <pablods90@users.noreply.github.com>
2020-04-07 23:49:25 +0100

    Configuring CUDA Drivers

--- ec2-gpu-docker/Readme.md
+++ ec2-gpu-docker/Readme.md
@@ -1,5 +1,8 @@
 
+## CUDA Toolkit
+
+- https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1804&target_type=deblocal
+
 ## Nvidia drivers
 
 - nvidia installation guide: https://docs.nvidia.com/datacenter/tesla/tesla-installation-notes/index.html
-- nvidia driver: https://docs.nvidia.com/datacenter/tesla/tesla-installation-notes/index.html
--- ec2-gpu-docker/scripts/instanceSetup.sh
+++ ec2-gpu-docker/scripts/instanceSetup.sh
@@ -3,6 +3,32 @@
 # Update && upgrade
 apt-get update && apt-get -y upgrade
 
-# Install required packages
-echo - "Installing packages..."
-apt-get install -y htop docker.io docker-compose git mesa-utils
+# Install required and useful packages..
+echo -e "Installing packages..."
+apt-get install -y  linux-headers-$(uname -r) \
+                    htop \
+                    docker.io \
+                    docker-compose \
+                    git \
+                    mesa-utils \
+                    gcc \
+
+###
+# Install NVIDIA Tesla Driver and CUDA Toolkit
+##
+echo -e "Install NVIDIA drivers..."
+
+echo -e "Retrieving the repository..."
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+curl -o /etc/apt/preferences.d/cuda-repository-pin-600 \
+        https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+
+echo -e "Installing the repository..."
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+
+echo -e "Update repositories ..."
+apt-get update
+
+echo -e "Install driver ..."
+apt-get -y install cuda-drivers cuda
